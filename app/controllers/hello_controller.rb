7470fb56133f81540912175cafddb2c3f3712138
sol <5o1.3an9@gmail.com>
2017-02-11 05:19:39 +0000

    study part 2

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,16 @@ class HelloController < ApplicationController
   def index
     render text: 'Hello World..!'
   end
+  
+  def view
+    @msg = 'Hello World!'
+  end
+  
+  def list
+    @books = Book.all
+  end
+  
+  def app_var
+    render text: MY_APP['logo']['source']
+  end
 end
