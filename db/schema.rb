a6943e89007db9312446fa199303478aaa87264a
sol <5o1.3an9@gmail.com>
2017-03-01 07:14:26 +0000

    make scafolding for part 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211035933) do
+ActiveRecord::Schema.define(version: 20170301015543) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
